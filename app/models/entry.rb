427e7acb55f908726080736ae1b8ce3973df9bfa
Alex McHale <alexmchale@gmail.com>
2009-05-10 08:55:39 -0500

    Add support for filtering the content by regex.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,12 +3,25 @@ class Entry < ActiveRecord::Base
 
   before_save :adjust_tags
 
+  FILTERS = {
+    'url' => /((http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?)$/ix,
+    'ups' => /\b(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|[\dT]\d\d\d ?\d\d\d\d ?\d\d\d)\b/i,
+    'fedex' => /\b(\d\d\d\d ?\d\d\d\d ?\d\d\d\d)\b/i
+  }
+
   def self.find_by_tag(user_id, tag)
     find :all, :conditions => [ 'user_id = ? AND tags LIKE ?', user_id, "% " + tag + " %" ]
   end
 
   def tags_list
     self.tags.to_s.split(/\s+/).find_all {|tag| !tag.blank?}
+  end
+
+  def filter(type)
+    regex = FILTERS[type.to_s] || /()/
+    result = content.to_s.scan(regex)
+    result.map! {|r| r.first.to_s.chomp}
+    result.find_all {|r| !r.blank?}
   end
 
 private
