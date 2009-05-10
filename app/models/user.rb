79bcced4b0f78c9215b0cddd54b6189858aa27bf
Alex McHale <alexmchale@gmail.com>
2009-05-09 23:06:02 -0500

    Add hashed authentication to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ActiveRecord::Base
+  before_save :rehash_password
+
+  def self.authenticate(username, password)
+    User.find :first,
+              :conditions => { :username => username,
+                               :password => hash_password(username, password) }
+  end
+
+private
+
+  def rehash_password
+    if username_changed? || password_changed?
+      self.password = User.hash_password(username, password)
+    end
+  end
+
+  def self.hash_password(username, password)
+    Digest::MD5.hexdigest("!!! %s WITH MY SALT %s !!!" % [ username, password ])
+  end
 end
