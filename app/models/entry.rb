078e18e61e2bdeb4fe0d589505a26b7fd7a5628b
Alex McHale <alexmchale@gmail.com>
2009-05-10 00:10:16 -0500

    Add tag adjuster to entry model.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,16 @@
 class Entry < ActiveRecord::Base
   belongs_to :user
+
+  before_save :adjust_tags
+
+  def tags_list
+    self.tags.to_s.split(/\s+/).find_all {|tag| !tag.blank?}
+  end
+
+private
+
+  def adjust_tags
+    self.tags = tags_list unless tags.kind_of? Array
+    self.tags = tags.join(' ').gsub(/\s+/, ' ').strip
+  end
 end
