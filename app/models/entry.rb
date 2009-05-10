ee7cf46aaa22cf7a0e2295837a752363d95ecd8f
Alex McHale <alexmchale@gmail.com>
2009-05-10 00:40:29 -0500

    Add utility method to find all entries that contain a given tag.  Add surrounding whitespace to all tags entries to allow simplified searching.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,6 +3,10 @@ class Entry < ActiveRecord::Base
 
   before_save :adjust_tags
 
+  def self.find_by_tag(user_id, tag)
+    find :all, :conditions => [ 'user_id = ? AND tags LIKE ?', user_id, "% " + tag + " %" ]
+  end
+
   def tags_list
     self.tags.to_s.split(/\s+/).find_all {|tag| !tag.blank?}
   end
@@ -12,5 +16,6 @@ private
   def adjust_tags
     self.tags = tags_list unless tags.kind_of? Array
     self.tags = tags.join(' ').gsub(/\s+/, ' ').strip
+    self.tags = ' ' + tags + ' ' # We always pad spaces to make searching by tag easier.
   end
 end
