99b5373932b71a45a7d300524c81a4e6be938ad9
Alex McHale <alexmchale@gmail.com>
2009-05-10 00:41:34 -0500

    Add link_entry_tags helper to create a list of links to the tags in a given entry.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def link_entry_tags(entry)
+    entry.tags_list.map do |tag|
+      link_to tag, :controller => :entries, :action => :index, :tag => tag
+    end.join ' '
+  end
 end
