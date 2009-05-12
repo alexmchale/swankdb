c955a82ccdbc93ac693f6d692685cf2d6710b255
Alex McHale <alexmchale@gmail.com>
2009-05-12 16:45:36 -0500

    Focus on the content pane and try to position the cursor at the end of the box.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function setCaretTo(obj, pos) {
+  if (obj.setSelectionRange) {
+    /* Gecko is a little bit shorter on that. Simply
+       focus the element and set the selection to a
+       specified position */
+    obj.focus();
+    obj.setSelectionRange(pos, pos);
+  } else if(obj.createTextRange) {
+    /* Create a TextRange, set the internal pointer to
+       a specified position and show the cursor at this
+       position */
+    var range = obj.createTextRange();
+    range.move("character", pos);
+    range.select();
+  }
+}
