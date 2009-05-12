90b16589a5e2b85adae3d612abf03314455e044f
Alex McHale <alexmchale@gmail.com>
2009-05-12 16:20:52 -0500

    Always downcase tag names before saving.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,12 @@
 class Tag < ActiveRecord::Base
   belongs_to :user
   belongs_to :entry
+
+  before_save :downcase_name
+
+private
+
+  def downcase_name
+    self.name = name.downcase if name
+  end
 end
