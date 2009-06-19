c2c429ca1a8a043b5413ec22c9c6ea579790d803
Alex McHale <alexmchale@gmail.com>
2009-06-18 23:06:40 -0500

    Added suggest feature to Tag model and improved saving.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,9 +4,22 @@ class Tag < ActiveRecord::Base
 
   before_save :downcase_name
 
+  def self.suggest(keyword, limit = 20)
+    keyword = keyword.gsub(/\\/, '\&\&').gsub(/'/, "''")
+    limit = limit.to_i
+
+    ActiveRecord::Base.connection.execute("
+      SELECT COUNT(*) AS count, name
+      FROM tags
+      WHERE name LIKE '%s%%'
+      GROUP BY name ORDER BY count DESC LIMIT %d
+    " % [ keyword, limit ])
+  end
+
 private
 
   def downcase_name
-    self.name = name.downcase if name
+    self.name = name.strip.downcase if name
+    self.readonly! if self.name.blank?
   end
 end
