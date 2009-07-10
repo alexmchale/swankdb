e7b67554184c20d9de42587889122f1568f68502
Alex McHale <alexmchale@gmail.com>
2009-07-10 03:52:32 +0000

    Add an about controller to provide staticy kinds of pages.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ ActionController::Routing::Routes.draw do |map|
   map.connect 'users/logout', :controller => 'users', :action => 'logout'
   map.resources :entries
   map.resources :users
+  map.resources :about
 
   # The priority is based upon order of creation: first created -> highest priority.
 
@@ -37,7 +38,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => 'entries'
+  map.root :controller => 'about'
 
   # See how all your routes lay out with "rake routes"
 
