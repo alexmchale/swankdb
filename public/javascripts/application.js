24c627d4c651a92b698e867df784180936623faf
Alex McHale <alexmchale@gmail.com>
2009-07-12 16:18:36 +0000

    Add a useful documentSize() function to the javascript.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,3 +14,17 @@ function setCaretTo(obj, pos) {
     range.select();
   }
 }
+
+function documentSize() {
+  var dimensions = {width: 0, height: 0};
+
+  if (document.documentElement) {
+    dimensions.width = document.documentElement.offsetWidth;
+    dimensions.height = document.documentElement.offsetHeight ;
+  } else if (window.innerWidth && window.innerHeight) {
+    dimensions.width = window.innerWidth;
+    dimensions.height = window.innerHeight;
+  }
+
+  return dimensions;
+}
