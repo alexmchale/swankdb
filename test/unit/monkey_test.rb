fe13792a9ef3f9a85790815e1a07d2281dba48b8
Alex McHale <alexmchale@gmail.com>
2009-07-14 21:06:08 +0000

    Add some unit testing for monkeypatches.

---
+++ test/unit/monkey_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class MonkeyTest < ActiveSupport::TestCase
+  test "generating html nonbreak spaces from a fixnum" do
+    assert_equal '', -1.spaces
+    assert_equal '', 0.spaces
+    assert_equal '&nbsp;', 1.spaces
+    assert_equal '&nbsp;&nbsp;', 2.spaces
+    assert_equal '&nbsp;&nbsp;&nbsp;', 3.spaces
+  end
+
+  test "stripping html from a string" do
+    assert_equal "hello\nworld", "hello<br>world".striphtml
+  end
+end
