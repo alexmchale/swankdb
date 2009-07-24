2e411722c7cb061278dfb8ae8a51ca296d36cb45
Alex McHale <alexmchale@gmail.com>
2009-07-24 00:03:57 +0000

    Use active record store to store sessions in the database.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090716212039) do
+ActiveRecord::Schema.define(:version => 20090723201130) do
 
   create_table "active_codes", :force => true do |t|
     t.integer  "user_id"
@@ -35,6 +35,16 @@ ActiveRecord::Schema.define(:version => 20090716212039) do
     t.datetime "updated_at"
     t.string   "tags"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "swank_logs", :force => true do |t|
     t.string   "code"
