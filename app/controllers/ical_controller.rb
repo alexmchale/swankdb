5d7d0e11b70a02bf7613b37c25fa8725bf39421e
Alex McHale <alexmchale@gmail.com>
2009-08-08 09:53:10 +0000

    Fix the way dates are presented in the iCal feed.

--- app/controllers/ical_controller.rb
+++ app/controllers/ical_controller.rb
@@ -30,13 +30,20 @@ private
 
       stripped = RDiscount.new(entry.content.to_s).to_html.striphtml
 
-      event.start = ((mode == :created) ? entry.created_at : entry.updated_at).to_date
-      event.summary = stripped.split(/\n+/).first.to_s[0, 25]
-      event.description = entry.content
+      event.dtstart = ((mode == :created) ? entry.created_at : entry.updated_at).to_date
+      event.dtstart.ical_params = { "VALUE" => "DATE" }
+      event.dtend = ((mode == :created) ? entry.created_at : entry.updated_at).to_date + 1
+      event.dtend.ical_params = { "VALUE" => "DATE" }
+      event.summary = clean_text(stripped.split(/\n+/).first.to_s[0, 25])
+      event.description = clean_text(entry.content)
 
       cal.add event
     end
 
     render :text => cal.to_ical
+  end
+
+  def clean_text(s)
+    s.to_s.gsub(/[^[:print:]\n]/, '')
   end
 end
