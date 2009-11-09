9a245ae91f11fbabf91cce2128ca7cec706ddf92
Alex McHale <alexmchale@gmail.com>
2009-11-09 13:01:04 +0000

    Added missing user email model methods.

--- app/models/user_emails.rb
+++ app/models/user_emails.rb
@@ -1,6 +1,6 @@
 class UserEmails < ActionMailer::Base
   def invitation(user, destination, message, subject = nil)
-    @recipients = destination.to_s.strip
+    @recipients = destination
     @subject = subject || "#{user.andand.username.to_s.capitalize} invites you to try SwankDB"
 
     @body = {
@@ -9,6 +9,27 @@ class UserEmails < ActionMailer::Base
       :message => message.to_s.strip
     }
 
+    @headers['reply-to'] = user.andand.email unless user.andand.email.blank?
+  end
+
+  def password_reset_request(user, reset_url)
+    @recipients = user.email
+    @subject = 'Password reset requested on SwankDB'
+
+    @body = {
+      :reset_url => reset_url
+    }
+  end
+
+  def entry(user, destination, subject, entry)
+    @recipients = destination
+    @subject = subject || "A Swank Note from #{user.andand.username.to_s.capitalize}"
+
+    @body = {
+      :content => entry.andand.render
+    }
+
+    @content_type = 'text/html'
     @headers['reply-to'] = user.andand.email unless user.andand.email.blank?
   end
 end
